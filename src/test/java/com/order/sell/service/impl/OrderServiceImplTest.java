fd8a55411d5c5e00c85b662ce716315b27a7e504
Andy Wang <wang64192664@gmail.com>
2023-04-15 22:59:51 -0800

    order test completed

---
+++ src/test/java/com/order/sell/service/impl/OrderServiceImplTest.java
@@ -0,0 +1,97 @@
+package com.order.sell.service.impl;
+
+import com.order.sell.dataobject.OrderDetail;
+import com.order.sell.dto.OrderDTO;
+import com.order.sell.enums.OrderStatusEnum;
+import com.order.sell.enums.PayStatusEnum;
+import junit.framework.TestCase;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class OrderServiceImplTest {
+
+    @Autowired
+    private OrderServiceImpl orderService;
+
+    private final String BUYER_OPENID = "1101110";
+
+    private final String ORDER_ID = "1681627639194523709";
+
+    @Test
+    public void create() throws Exception {
+
+        OrderDTO orderDTO = new OrderDTO();
+        orderDTO.setBuyerName("Andy");
+        orderDTO.setBuyerAddress("2106 Desoto");
+        orderDTO.setBuyerPhone("123456789012");
+        orderDTO.setBuyerOpenid(BUYER_OPENID);
+
+        //shopping cart
+        List<OrderDetail> orderDetailList = new ArrayList<>();
+        OrderDetail o1 = new OrderDetail();
+        o1.setProductId("123456");
+        o1.setProductQuantity(1);
+
+        OrderDetail o2 = new OrderDetail();
+        o2.setProductId("123457");
+        o2.setProductQuantity(2);
+
+        orderDetailList.add(o1);
+        orderDetailList.add(o2);
+
+        orderDTO.setOrderDetailList(orderDetailList);
+
+        OrderDTO result = orderService.create(orderDTO);
+        log.info("[Order created] result={}", result);
+        Assert.assertNotNull(result);
+    }
+
+    @Test
+    public void findOne() throws Exception {
+        OrderDTO result = orderService.findOne(ORDER_ID);
+        log.info("[look for single order]result={}", result);
+        Assert.assertEquals(ORDER_ID, result.getOrderId());
+    }
+
+    @Test
+    public void findList() throws Exception {
+        PageRequest request = new PageRequest(0,2);
+        Page<OrderDTO> orderDTOPage = orderService.findList(BUYER_OPENID, request);
+        Assert.assertNotEquals(0, orderDTOPage.getTotalElements());
+    }
+
+    @Test
+    public void cancel() throws Exception {
+        OrderDTO orderDTO = orderService.findOne(ORDER_ID);
+        OrderDTO result = orderService.cancel(orderDTO);
+        Assert.assertEquals(OrderStatusEnum.CANCEL.getCode(), result.getOrderStatus());
+    }
+
+    @Test
+    public void finish() throws Exception {
+        OrderDTO orderDTO = orderService.findOne(ORDER_ID);
+        OrderDTO result = orderService.finish(orderDTO);
+        Assert.assertEquals(OrderStatusEnum.FINISHED.getCode(), result.getOrderStatus());
+    }
+
+    @Test
+    public void paid() throws Exception {
+        OrderDTO orderDTO = orderService.findOne(ORDER_ID);
+        OrderDTO result = orderService.paid(orderDTO);
+        Assert.assertEquals(PayStatusEnum.SUCCESS.getCode(), result.getPayStatus());
+    }
+
+}
