2c5dd725fbe6c8116a932a3c99258fb1827b4d57
Andy Wang <wang64192664@gmail.com>
2023-04-22 12:41:03 -0800

    add payservice test

---
+++ src/test/java/com/order/sell/service/impl/PayServiceImplTest.java
@@ -0,0 +1,30 @@
+package com.order.sell.service.impl;
+
+import com.order.sell.dto.OrderDTO;
+import com.order.sell.service.OrderService;
+import com.order.sell.service.PayService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class PayServiceImplTest {
+
+    @Autowired
+    private PayService payService;
+
+    @Autowired
+    private OrderService orderService;
+
+    @Test
+    public void create() throws Exception {
+        OrderDTO orderDTO = orderService.findOne("1681630754912127521");
+        payService.create(orderDTO);
+    }
+
+}
