bd3cd7cdb343ead34c553f08965841cdb7bd7b43
Andy Wang <wang64192664@gmail.com>
2023-04-22 12:39:15 -0800

    add config

--- src/main/java/com/order/sell/config/WechatAccountConfig.java
+++ src/main/java/com/order/sell/config/WechatAccountConfig.java
@@ -13,4 +13,21 @@ public class WechatAccountConfig {
     private String mpAppId;
 
     private String mpAppSecret;
+
+    /**
+     * 商户号
+     */
+    private String mchId;
+
+    /**
+     * 商户密钥
+     */
+    private String mchKey;
+
+    /**
+     * 商户证书路径
+     */
+    private String keyPath;
+
+    private String notifyUrl;
 }
