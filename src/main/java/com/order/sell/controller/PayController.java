435f07be6e30515bb206517e72ebaea922db2a5d
Andy Wang <wang64192664@gmail.com>
2023-04-22 12:39:37 -0800

    pay controller

---
+++ src/main/java/com/order/sell/controller/PayController.java
@@ -0,0 +1,31 @@
+package com.order.sell.controller;
+
+import com.order.sell.dto.OrderDTO;
+import com.order.sell.enums.ResultEnum;
+import com.order.sell.exception.SellException;
+import com.order.sell.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/pay")
+public class PayController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping("/create")
+    public void create(@RequestParam("orderId") String orderId,
+                       @RequestParam("returnUrl") String returnUrl) {
+        //1. 查询订单
+        OrderDTO orderDTO = orderService.findOne(orderId);
+        if (orderDTO == null) {
+            throw new SellException(ResultEnum.ORDER_NOT_EXIST);
+        }
+
+        //发起支付
+    }
+}
