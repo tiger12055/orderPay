8d48ac00e577300cdb4a4dc49c4d6e1077dec1cb
Andy Wang <wang64192664@gmail.com>
2023-04-22 22:34:45 -0800

    update payservice to include refund function

--- src/main/java/com/order/sell/service/PayService.java
+++ src/main/java/com/order/sell/service/PayService.java
@@ -1,8 +1,14 @@
 package com.order.sell.service;
 
+import com.lly835.bestpay.model.PayResponse;
+import com.lly835.bestpay.model.RefundResponse;
 import com.order.sell.dto.OrderDTO;
 
 public interface PayService {
 
-    void create(OrderDTO orderDTO);
+    PayResponse create(OrderDTO orderDTO);
+
+    PayResponse notify(String notifyData);
+
+    RefundResponse refund(OrderDTO orderDTO);
 }
--- src/main/java/com/order/sell/service/impl/PayServiceImpl.java
+++ src/main/java/com/order/sell/service/impl/PayServiceImpl.java
@@ -3,10 +3,16 @@ package com.order.sell.service.impl;
 import com.lly835.bestpay.enums.BestPayTypeEnum;
 import com.lly835.bestpay.model.PayRequest;
 import com.lly835.bestpay.model.PayResponse;
+import com.lly835.bestpay.model.RefundRequest;
+import com.lly835.bestpay.model.RefundResponse;
 import com.lly835.bestpay.service.impl.BestPayServiceImpl;
 import com.order.sell.dto.OrderDTO;
+import com.order.sell.enums.ResultEnum;
+import com.order.sell.exception.SellException;
+import com.order.sell.service.OrderService;
 import com.order.sell.service.PayService;
 import com.order.sell.utils.JsonUtil;
+import com.order.sell.utils.MathUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,8 +26,11 @@ public class PayServiceImpl implements PayService {
     @Autowired
     private BestPayServiceImpl bestPayService;
 
+    @Autowired
+    private OrderService orderService;
+
     @Override
-    public void create(OrderDTO orderDTO) {
+    public PayResponse create(OrderDTO orderDTO) {
         PayRequest payRequest = new PayRequest();
         payRequest.setOpenid(orderDTO.getBuyerOpenid());
         payRequest.setOrderAmount(orderDTO.getOrderAmount().doubleValue());
@@ -32,5 +41,54 @@ public class PayServiceImpl implements PayService {
 
         PayResponse payResponse = bestPayService.pay(payRequest);
         log.info("[make wechat payment]response={}", JsonUtil.toJson(payResponse));
+        return payResponse;
+    }
+
+    @Override
+    public PayResponse notify(String notifyData) {
+        //1. 验证签名
+        //2. 支付的状态
+        //3. 支付金额
+        //4. 支付人(下单人 == 支付人)
+
+        PayResponse payResponse = bestPayService.asyncNotify(notifyData);
+        log.info("【微信支付】异步通知, payResponse={}", JsonUtil.toJson(payResponse));
+
+        //查询订单
+        OrderDTO orderDTO = orderService.findOne(payResponse.getOrderId());
+
+        //判断订单是否存在
+        if (orderDTO == null) {
+            log.error("【微信支付】异步通知, 订单不存在, orderId={}", payResponse.getOrderId());
+            throw new SellException(ResultEnum.ORDER_NOT_EXIST);
+        }
+
+        //判断金额是否一致(0.10   0.1)
+        if (!MathUtil.equals(payResponse.getOrderAmount(), orderDTO.getOrderAmount().doubleValue())) {
+            log.error("【微信支付】异步通知, 订单金额不一致, orderId={}, 微信通知金额={}, 系统金额={}",
+                    payResponse.getOrderId(),
+                    payResponse.getOrderAmount(),
+                    orderDTO.getOrderAmount());
+            throw new SellException(ResultEnum.WXPAY_NOTIFY_MONEY_VERIFY_ERROR);
+        }
+
+        //修改订单的支付状态
+        orderService.paid(orderDTO);
+
+        return payResponse;
+    }
+
+    @Override
+    public RefundResponse refund(OrderDTO orderDTO) {
+        RefundRequest refundRequest = new RefundRequest();
+        refundRequest.setOrderId(orderDTO.getOrderId());
+        refundRequest.setOrderAmount(orderDTO.getOrderAmount().doubleValue());
+        refundRequest.setPayTypeEnum(BestPayTypeEnum.WXPAY_H5);
+        log.info("[Wechat refund] request={}", JsonUtil.toJson(refundRequest));
+
+        RefundResponse refundResponse = bestPayService.refund(refundRequest);
+        log.info("[Wechat refund] response={}", JsonUtil.toJson(refundResponse));
+
+        return refundResponse;
     }
 }
