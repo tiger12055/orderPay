c1db1ed1697b9150a66c03f1b4f1585e1c470148
Andy Wang <wang64192664@gmail.com>
2023-04-24 23:21:08 -0800

    add test cases

--- src/test/java/com/order/sell/service/impl/ProductServiceImplTest.java
+++ src/test/java/com/order/sell/service/impl/ProductServiceImplTest.java
@@ -57,4 +57,16 @@ public class ProductServiceImplTest {
         Assert.assertNotNull(result);
     }
 
+    @Test
+    public void onSale() {
+        ProductInfo result = productService.onSale("123456");
+        Assert.assertEquals(ProductStatusEnum.UP, result.getProductStatusEnum());
+    }
+
+    @Test
+    public void offSale() {
+        ProductInfo result = productService.offSale("123456");
+        Assert.assertEquals(ProductStatusEnum.DOWN, result.getProductStatusEnum());
+    }
+
 }
