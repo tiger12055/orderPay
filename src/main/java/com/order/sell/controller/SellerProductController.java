da2d32edb57805370c6b40803a78bcd33e286cb8
Andy Wang <wang64192664@gmail.com>
2023-04-24 23:23:13 -0800

    sellerproductcontroller completed

---
+++ src/main/java/com/order/sell/controller/SellerProductController.java
@@ -0,0 +1,83 @@
+package com.order.sell.controller;
+
+import com.order.sell.dataobject.ProductInfo;
+import com.order.sell.exception.SellException;
+import com.order.sell.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping("/seller/product")
+public class SellerProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    /**
+     * 列表
+     * @param page
+     * @param size
+     * @param map
+     * @return
+     */
+    @GetMapping("/list")
+    public ModelAndView list(@RequestParam(value = "page", defaultValue = "1") Integer page,
+                             @RequestParam(value = "size", defaultValue = "10") Integer size,
+                             Map<String, Object> map) {
+        PageRequest request = new PageRequest(page - 1, size);
+        Page<ProductInfo> productInfoPage = productService.findAll(request);
+        map.put("productInfoPage", productInfoPage);
+        map.put("currentPage", page);
+        map.put("size", size);
+        return new ModelAndView("product/list", map);
+    }
+
+    /**
+     * product on sale
+     * @param productId
+     * @param map
+     * @return
+     */
+    @RequestMapping("/on_sale")
+    public ModelAndView onSale(@RequestParam("productId") String productId,
+                               Map<String, Object> map) {
+        try {
+            productService.onSale(productId);
+        } catch (SellException e) {
+            map.put("msg", e.getMessage());
+            map.put("url", "/sell/seller/product/list");
+            return new ModelAndView("common/error", map);
+        }
+
+        map.put("url", "/sell/seller/product/list");
+        return new ModelAndView("common/success", map);
+    }
+    /**
+     * product off sale
+     * @param productId
+     * @param map
+     * @return
+     */
+    @RequestMapping("/off_sale")
+    public ModelAndView offSale(@RequestParam("productId") String productId,
+                                Map<String, Object> map) {
+        try {
+            productService.offSale(productId);
+        } catch (SellException e) {
+            map.put("msg", e.getMessage());
+            map.put("url", "/sell/seller/product/list");
+            return new ModelAndView("common/error", map);
+        }
+
+        map.put("url", "/sell/seller/product/list");
+        return new ModelAndView("common/success", map);
+    }
+}
