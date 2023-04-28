a7862aeca3432cde45a25a16eacb61eca23a4677
Andy Wang <wang64192664@gmail.com>
2023-04-27 22:14:28 -0800

    add sellerproduct logic

--- src/main/java/com/order/sell/controller/SellerProductController.java
+++ src/main/java/com/order/sell/controller/SellerProductController.java
@@ -1,17 +1,27 @@
 package com.order.sell.controller;
 
+import com.order.sell.dataobject.ProductCategory;
 import com.order.sell.dataobject.ProductInfo;
 import com.order.sell.exception.SellException;
+import com.order.sell.form.ProductForm;
+import com.order.sell.service.CategoryService;
 import com.order.sell.service.ProductService;
+import com.order.sell.utils.KeyUtil;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -20,6 +30,9 @@ public class SellerProductController {
 
     @Autowired
     private ProductService productService;
+
+    @Autowired
+    private CategoryService categoryService;
 
     /**
      * 列表
@@ -74,6 +87,51 @@ public class SellerProductController {
         } catch (SellException e) {
             map.put("msg", e.getMessage());
             map.put("url", "/sell/seller/product/list");
+            return new ModelAndView("common/error", map);
+        }
+
+        map.put("url", "/sell/seller/product/list");
+        return new ModelAndView("common/success", map);
+    }
+
+    @GetMapping("/index")
+    public ModelAndView index(@RequestParam(value = "productId", required = false) String productId,
+                              Map<String, Object> map) {
+        if (!StringUtils.isEmpty(productId)) {
+            ProductInfo productInfo = productService.findOne(productId);
+            map.put("productInfo", productInfo);
+        }
+
+        //look for all category
+        List<ProductCategory> categoryList = categoryService.findAll();
+        map.put("categoryList", categoryList);
+
+        return new ModelAndView("product/index", map);
+    }
+
+    @PostMapping("/save")
+    public ModelAndView save(@Valid ProductForm form,
+                             BindingResult bindingResult,
+                             Map<String, Object> map) {
+        if (bindingResult.hasErrors()) {
+            map.put("msg", bindingResult.getFieldError().getDefaultMessage());
+            map.put("url", "/sell/seller/product/index");
+            return new ModelAndView("common/error", map);
+        }
+
+        ProductInfo productInfo = new ProductInfo();
+        try {
+            //if productId is empty, it is an addition
+            if (!StringUtils.isEmpty(form.getProductId())) {
+                productInfo = productService.findOne(form.getProductId());
+            } else {
+                form.setProductId(KeyUtil.genUniqueKey());
+            }
+            BeanUtils.copyProperties(form, productInfo);
+            productService.save(productInfo);
+        } catch (SellException e) {
+            map.put("msg", e.getMessage());
+            map.put("url", "/sell/seller/product/index");
             return new ModelAndView("common/error", map);
         }
 
