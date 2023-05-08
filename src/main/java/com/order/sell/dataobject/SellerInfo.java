808189b3535866b209de1b93d9b72d874a434388
Andy Wang <wang64192664@gmail.com>
2023-05-07 22:11:24 -0800

    add new obj

--- src/main/java/com/order/sell/dataobject/SellerInfo.java
+++ src/main/java/com/order/sell/dataobject/SellerInfo.java
@@ -1,2 +1,20 @@
-package com.order.sell.dataobject;public class SellerInfo {
+package com.order.sell.dataobject;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class SellerInfo {
+
+    @Id
+    private String sellerId;
+
+    private String username;
+
+    private String password;
+
+    private String openid;
 }
