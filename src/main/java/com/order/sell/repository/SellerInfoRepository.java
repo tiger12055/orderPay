356c8c02e2a7b9364f4be79580c90e49dab562cd
Andy Wang <wang64192664@gmail.com>
2023-05-07 22:11:43 -0800

    add new repo

--- src/main/java/com/order/sell/repository/SellerInfoRepository.java
+++ src/main/java/com/order/sell/repository/SellerInfoRepository.java
@@ -1,2 +1,8 @@
-package com.order.sell.repository;public class SellerInfoRepository {
+package com.order.sell.repository;
+
+import com.order.sell.dataobject.SellerInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SellerInfoRepository extends JpaRepository<SellerInfo, String> {
+    SellerInfo findByOpenid(String openid);
 }
