413624bc06b40d760a616d432dd2ce1d1df5b12b
Andy Wang <wang64192664@gmail.com>
2023-05-07 22:12:46 -0800

    add service interface

--- src/main/java/com/order/sell/service/SellerService.java
+++ src/main/java/com/order/sell/service/SellerService.java
@@ -1,2 +1,8 @@
-package com.order.sell.service;public class SellerService {
+package com.order.sell.service;
+
+import com.order.sell.dataobject.SellerInfo;
+
+public interface SellerService {
+
+    SellerInfo findSellerInfoByOpenid(String openid);
 }
